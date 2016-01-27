479c01778917a93079364f0898647eb4a9cde05c
Chris Mackey <chris@mackeyarchitecture.com>
2016-01-27 16:04:21 -0500

    Fixed some Bugs and added Test page

--- static/js/comfortModels.js
+++ static/js/comfortModels.js
@@ -14,9 +14,11 @@ var outdoorTemp = -9
 var wallRVal = 5
 var minAcceptMRT = 20.87
 var filmCoeff = 8.29
+var opaqueViewFacs =[0.359653,0.261388,0.219306,0.178742,0.155965,0.137527,0.121041,0.108894,0.096529,0.087636,0.080694,0.079176]
+var winViewFacs = [0.03167,0.051193,0.040781,0.040347,0.033839,0.0282,0.024729,0.020824,0.021692,0.02039,0.016486,0.01128]
 
 
-// Functions that compute U-Values acceptable with MRT threshold.
+// Function that computes U-Values acceptable with MRT threshold.
 
 comf.calcOpaqueTemp = function(airTemp, outTemp, wallR) {
     return airTemp-(((1/wallR)*(airTemp-outTemp))/8.29);
@@ -35,22 +37,21 @@ var opaqueTemp = comf.calcOpaqueTemp(airTemp, outdoorTemp, wallRVal)
 
 var opaqueContrib = []
 for (var i = 0; i < opaqueViewFacs.length; i++) {
-	var opaViewFac_i = opaqueViewFacs[i];
-	var winViewFac_i = winViewFacs[i]];
-	var opaqContrib_i = comf.MRTCondtribOfOpaque(opaqueTemp, opaViewFac_i, winViewFac_i, airTemp);
-	opaqueContrib.push(opaqContrib_i);
+  var opaViewFac_i = opaqueViewFacs[i];
+  var winViewFac_i = winViewFacs[i];
+  var opaqContrib_i = comf.MRTCondtribOfOpaque(opaqueTemp, opaViewFac_i, winViewFac_i, airTemp);
+  opaqueContrib.push(opaqContrib_i);
 }
 
 
 var UVals = []
 for (var i = 0; i < winViewFacs.length; i++) {
-	var winViewFac_i = winViewFacs[i]];
-	var opaqContrib_i = opaqueContrib[i];
-	var UMax = comf.maxUOfMRT(minAcceptMRT, airTemp, outdoorTemp, winViewFac_i, opaqContrib_i, filmCoeff);
-	if (filmCoeff > 5) {
-		UVals.push(UMax);
-	} else {
-		UVals.push(UMax/0.2);
-	}
+  var winViewFac_i = winViewFacs[i];
+  var opaqContrib_i = opaqueContrib[i];
+  var UMax = comf.maxUOfMRT(minAcceptMRT, airTemp, outdoorTemp, winViewFac_i, opaqContrib_i, filmCoeff);
+  if (filmCoeff > 5) {
+    UVals.push(UMax);
+  } else {
+    UVals.push(UMax/0.2);
+  }
 }
-
